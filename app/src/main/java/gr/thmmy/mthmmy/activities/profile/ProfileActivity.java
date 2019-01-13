bd436f8e641c844494ace45be5b73ff9980c2689
Apostolof <apotwohd@gmail.com>
2019-01-13 13:29:26 +0200

    Init profile expandable avatar

--- app/src/main/java/gr/thmmy/mthmmy/activities/profile/ProfileActivity.java
+++ app/src/main/java/gr/thmmy/mthmmy/activities/profile/ProfileActivity.java
@@ -1,7 +1,13 @@
 package gr.thmmy.mthmmy.activities.profile;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -13,6 +19,7 @@ import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -83,6 +90,9 @@ public class ProfileActivity extends BaseActivity implements LatestPostsFragment
 
     private TextView usernameView;
     private ImageView avatarView;
+    private ImageView expandedImageView;
+    private Animator currentAnimator;
+    private int shortAnimationDuration;
     private TextView personalTextView;
     private MaterialProgressBar progressBar;
     private FloatingActionButton pmFAB;
@@ -127,11 +137,16 @@ public class ProfileActivity extends BaseActivity implements LatestPostsFragment
         progressBar = findViewById(R.id.progressBar);
 
         avatarView = findViewById(R.id.user_thumbnail);
-        if (!Objects.equals(avatarUrl, ""))
+        expandedImageView = findViewById(R.id.expanded_user_thumbnail);
+        if (!Objects.equals(avatarUrl, "")) {
             //noinspection ConstantConditions
             loadAvatar();
-        else
+            avatarView.setOnClickListener(view -> zoomImageFromThumb(avatarView));
+            shortAnimationDuration = getResources().getInteger(
+                    android.R.integer.config_shortAnimTime);
+        } else
             loadDefaultAvatar();
+
         usernameView = findViewById(R.id.profile_activity_username);
         usernameView.setTypeface(Typeface.createFromAsset(this.getAssets()
                 , "fonts/fontawesome-webfont.ttf"));
@@ -212,7 +227,7 @@ public class ProfileActivity extends BaseActivity implements LatestPostsFragment
         if (pmFAB.getVisibility() != View.GONE) pmFAB.setEnabled(false);
     }
 
-    private void loadAvatar(){
+    private void loadAvatar() {
         Picasso.with(this)
                 .load(avatarUrl)
                 .fit()
@@ -223,9 +238,19 @@ public class ProfileActivity extends BaseActivity implements LatestPostsFragment
                         , R.drawable.ic_default_user_avatar, null)))
                 .transform(new CircleTransform())
                 .into(avatarView);
+
+        Picasso.with(this)
+                .load(avatarUrl)
+                .fit()
+                .centerCrop()
+                .error(Objects.requireNonNull(ResourcesCompat.getDrawable(this.getResources()
+                        , R.drawable.ic_default_user_avatar, null)))
+                .placeholder(Objects.requireNonNull(ResourcesCompat.getDrawable(this.getResources()
+                        , R.drawable.ic_default_user_avatar, null)))
+                .into(expandedImageView);
     }
 
-    private void loadDefaultAvatar(){
+    private void loadDefaultAvatar() {
         Picasso.with(this)
                 .load(R.drawable.ic_default_user_avatar)
                 .fit()
@@ -236,6 +261,144 @@ public class ProfileActivity extends BaseActivity implements LatestPostsFragment
                         , R.drawable.ic_default_user_avatar, null)))
                 .transform(new CircleTransform())
                 .into(avatarView);
+    }
+
+    /*
+     * Also check this out:
+     * https://stackoverflow.com/questions/39749404/android-shared-element-transition-transforming-an-imageview-from-a-circle-to-a
+     */
+    private void zoomImageFromThumb(final View thumbView) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.main_content)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f))
+                .with(ObjectAnimator.ofFloat(expandedImageView,
+                        View.SCALE_Y, startScale, 1f));
+        set.setDuration(shortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                currentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                currentAnimator = null;
+            }
+        });
+        set.start();
+        currentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentAnimator != null) {
+                    currentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y, startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(shortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        currentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        currentAnimator = null;
+                    }
+                });
+                set.start();
+                currentAnimator = set;
+            }
+        });
     }
 
     /**
